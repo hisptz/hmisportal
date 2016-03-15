b873b03d6ebc5f279ab639407aabb2cb78289b61
Leonard Mpande <leo.augut27@gmail.com>
2016-03-15 14:21:50 +0300

    relsolving distrct map bug

--- services/mapService.js
+++ services/mapService.js
@@ -34,10 +34,11 @@ angular.module("hmisPortal")
                 card.DistrictFreeObject = [];
                 console.log(JSON.stringify(valueTouseArray));
                 console.log(JSON.stringify(legend));
-                console.log(JSON.stringify(data.features));
+                //console.log(JSON.stringify(data.features));
 
                 angular.forEach(data.features, function (value, index) {
-                    console.log(JSON.stringify(data.features[index]));
+                    console.log("  ---  ");
+                    console.log(JSON.stringify(value));
                     var appropiateColor = decideOnColor(max_and_min,legend,value,index,valueTouseArray);
                     // creating dynamic colors for district
                     card.saveColorInlocalStorage(prepareId(card,value.id),appropiateColor.color);
