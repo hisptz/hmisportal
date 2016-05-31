51366e61680b3d9a915a773336e7a0da511e8daf
Vincent P. Minde <vincentminde@gmail.com>
2016-05-31 09:32:09 +0300

    Made changes

--- controllers/family_Planning/PDFControllers/stockoutCtrl.js
+++ controllers/family_Planning/PDFControllers/stockoutCtrl.js
@@ -239,7 +239,7 @@ angular.module("hmisPortal")
                         render.addRequest();
                         //$http.get(portalService.base+'api/sqlViews/vj6E3KoFP28/data.json?&'+FPManager.lastTwelveMonthForSql(FPManager.lastMonthWithData)).success(function(facilities){
                         alert(FPManager.lastMonthWithData);
-                        $http.get(portalService.base+'api/sqlViews/vj6E3KoFP28/data.json?&'+FPManager.lastTwelveMonthForSql("201511")).success(function(facilities){
+                        $http.get(portalService.base+'api/sqlViews/vj6E3KoFP28/data.json?&'+FPManager.lastTwelveMonthForSql("201412")).success(function(facilities){
                             var injecatbleRegions = {};
                             var oralRegions = {};
                             var injecatbleData = [];
